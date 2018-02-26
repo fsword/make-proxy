f14afaaf50bf775371be0e8b6eedef3cd5c622da
fsword <li.jianye@gmail.com>
2018-02-26 13:58:11 +0800

    set key by enviroment

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ COPY . /app
 WORKDIR /app
 RUN apk add -U git && ./rebar3 tree && ./rebar3 compile
 
-
+ENTRYPOINT ["/app/docker-entrypoint.sh"]
 
---
+++ docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ "$KEY" != "" ]
+then
+  sed -e "s/1234567890abcdef/$KEY/g" app.config.example > app.config
+fi
+
+erl -pa ./_build/default/lib/*/ebin +K true -config app -s make_proxy start_server
+
