4fb2c900b51ef32ae7c542d504af566954b9196d
fsword <li.jianye@gmail.com>
2018-02-26 14:29:30 +0800

    use exec

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,5 @@ then
   sed -e "s/1234567890abcdef/$KEY/g" app.config.example > app.config
 fi
 
-erl -pa ./_build/default/lib/*/ebin +K true -config app -s make_proxy start_server
+exec erl -pa ./_build/default/lib/*/ebin +K true -config app -s make_proxy start_server
 
