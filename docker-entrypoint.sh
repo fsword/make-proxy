b166c65c9887851aa7481661799c42b96bf734e4
fsword <li.jianye@gmail.com>
2019-09-25 10:24:11 +0800

    server port can be customized

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ "$KEY" != "" ]
-then
-  sed -e "s/1234567890abcdef/$KEY/g" app.config.example > app.config
-fi
+KEY=${KEY:="1234567890abcdef"}
+PORT=${PORT:="7071"}
+
+sed -e "s/your_key/$KEY/g" -e "s/7071/$PORT/g" app.config.example > app.config
 
 exec erl -pa ./_build/default/lib/*/ebin +K true -config app -s make_proxy start_server
 
